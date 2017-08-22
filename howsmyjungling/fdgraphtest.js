fbacc29fda7d3ee5ffa66ec38798228a39222b96
David R. Saeva <drsaeva@gmail.com>
2017-08-22 06:33:29 -0400

    Update fdgraphtest.js

--- howsmyjungling/fdgraphtest.js
+++ howsmyjungling/fdgraphtest.js
@@ -1,75 +1,171 @@
-"use strict";
+var network = {
+  // Start data
+  graph: {
+    "nodes":[
+      {"id":1,"name":"Jack Hayes","colour":"#567e7b","connections":2},
+      {"id":2,"name":"Amy Castillo","colour":"#85ed67","connections":2},
+      {"id":3,"name":"Kenneth Williams","colour":"#666b01","connections":1},
+      {"id":4,"name":"Phillip Hicks","colour":"#49d159","connections":3},
+      {"id":5,"name":"Ryan Ortiz","colour":"#47f1ee","connections":2},
+      {"id":6,"name":"Edward Moore","colour":"#05626d","connections":0},
+      {"id":7,"name":"Frances Gomez","colour":"#8ee92f","connections":2},
+      {"id":8,"name":"Julia Adams","colour":"#73e14b","connections":0},
+      {"id":9,"name":"Martha Montgomery","colour":"#0b9b89"}],
+    "links":[
+      {"source":8,"target":4},
+      {"source":5,"target":7},
+      {"source":3,"target":5},
+      {"source":2,"target":1},
+      {"source":7,"target":2},
+      {"source":6,"target":5},
+      {"source":6,"target":1},
+      {"source":8,"target":2},
+      {"source":8,"target":7},
+      {"source":7,"target":4},
+      {"source":1,"target":4},
+      {"source":1,"target":3}]
+  },
+  // Graph design
+  width: 500,
+  height: 500,
+  lines: {
+		stroke: {
+			color: "#ccc",
+			thickness: 2
+		}
+	},
+	nodes: {
+		fill: {
+			color: "#333"
+		},
+		stroke: {
+			color: "#fff",
+			thickness: 3
+		},
+		sizeRange: [8,30]
+	},
+	setup: function(){
+	  var $network = $("#network");
+	  
+	  $('<canvas/>').attr({
+			'id':'networkCanvas',
+			'width':this.width,
+			'height':this.height
+		}).appendTo($("#network"));
+		
+		this.canvas = document.getElementById('networkCanvas');
+		this.context = this.canvas.getContext('2d');
+		
+		this.simulation = d3.forceSimulation()
+			.stop()
+			.force("link",d3.forceLink().id((d)=>{return d.id}))
+			.force("change",d3.forceManyBody())
+			.force("center",d3.forceCenter())
+			.force("collide",d3.forceCollide().radius((d)=>{return d.force;}).iterations(2))
+			.on("tick",()=>{
+				this.ticked();
+			});
 
-var height = 800, width = 640;
-var svg = d3.select("#fdg").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+    this.drawData(this.graph);
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+	},
+	forceScale: function(node){
+    var scale = d3.scaleLog().domain(this.nodes.sizeRange).range(this.nodes.sizeRange.slice().reverse());
+    return node.r + scale(node.r);
+	},
+	drawData: function(graph){
+	  var countExtent = d3.extent(graph.nodes,function(d){return d.connections}),
+				radiusScale = d3.scalePow().exponent(2).domain(countExtent).range(this.nodes.sizeRange);
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    // Let D3 figure out the forces
+		for(var i=0,ii=graph.nodes.length;i<ii;i++) {
+			var node = graph.nodes[i];
 
-d3.json("miserables.json", function(error, graph) {
-  if (error) throw error;
+			node.r = radiusScale(node.connections);
+			node.force = this.forceScale(node);
+		};
 
-  var link = svg.append("g")
-      .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+    // Add new data to old data
+		this.graph.nodes = this.graph.nodes.concat(graph.nodes);
+		this.graph.links = this.graph.links.concat(graph.links);
 
-  var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d) { return color(d.group); })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+    // Feed to simulation
+		this.simulation
+			.nodes(this.graph.nodes);
 
-  node.append("title")
-      .text(function(d) { return d.id; });
+		this.simulation.force("link")
+			.links(this.graph.links);
 
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+		this.simulation.alpha(0.3).restart();
+	},
+	ticked: function(){
+	  if(!this.graph) {
+			return false;
+		}
 
-  simulation.force("link")
-      .links(graph.links);
+		this.context.clearRect(0,0,this.width,this.height);
+		this.context.save();
+		this.context.translate(this.width / 2, this.height / 2);
 
-  function ticked() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+		this.context.beginPath();
+		this.graph.links.forEach((d)=>{
+			this.context.moveTo(d.source.x, d.source.y);
+			this.context.lineTo(d.target.x, d.target.y);
+		});
+		this.context.strokeStyle = this.lines.stroke.color;
+		this.context.lineWidth = this.lines.stroke.thickness;
 
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  }
+		this.context.stroke();
+		
+		this.graph.nodes.forEach((d)=>{
+			this.context.beginPath();
+			
+			this.context.moveTo(d.x + d.r, d.y);
+			this.context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
+
+			this.context.fillStyle = d.colour;
+			this.context.strokeStyle =this.nodes.stroke.color;
+			this.context.lineWidth = this.nodes.stroke.thickness;
+			this.context.fill();
+			this.context.stroke();
+		});
+	
+		this.context.restore();
+	}
+	
+  
+};
+
+var lastId = 9;
+
+var getRandomColor = function(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+var addNode = function(){
+  var newGraph = {
+    nodes: [{
+      name: "Name-"+(++lastId),
+      id: lastId,
+      colour: getRandomColor(),
+      connections: 1
+    }],
+    links: [{
+      source: 1,
+      target: lastId
+    }]
+  };
+  console.log(newGraph);
+  network.drawData(newGraph);
+};
+
+$(function(){
+  network.setup();
+  
+  $("#addNode").click(addNode);
 });
-
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
-
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
