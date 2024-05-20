b18243e5227daf4d3874cb59199aea4427a273ef
David R Saeva <11340638+drsaeva@users.noreply.github.com>
2024-05-20 01:33:03 -0400

    init new nuxt project

---
+++ components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <NuxtLink to="#">home</NuxtLink>
+    <NuxtLink to="#about">about</NuxtLink>
+    <NuxtLink to="#projects">projects</NuxtLink>
+    <NuxtLink to="#latestpost">latest post</NuxtLink>
+    <NuxtLink to="#contact">contact</NuxtLink>
+  </nav>
+</template>
