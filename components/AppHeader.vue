27cd421c348a6d98ce0e9805fec2417b23b90fcf
David R. Saeva <11340638+drsaeva@users.noreply.github.com>
2025-09-13 15:33:23 -0400

    refactor as next app with ci/cd

--- components/AppHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav>
-    <NuxtLink to="#">home</NuxtLink>
-    <NuxtLink to="#about">about</NuxtLink>
-    <NuxtLink to="#projects">projects</NuxtLink>
-    <NuxtLink to="#latestpost">latest post</NuxtLink>
-    <NuxtLink to="#contact">contact</NuxtLink>
-  </nav>
-</template>
